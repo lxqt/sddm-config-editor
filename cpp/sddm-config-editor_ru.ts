0cbdd08f24dc96879e020b3d0e695a58ebccc636
Andrey Bondrov <andrey.bondrov@rosalab.ru>
2017-05-30 13:57:34 +1000

    Add Russian translation

---
+++ cpp/sddm-config-editor_ru.ts
@@ -0,0 +1,303 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>config-strings</name>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="3"/>
+        <source>Autologin</source>
+        <translation>Автовход</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="6"/>
+        <source>Whether sddm should automatically log back into sessions when they exit</source>
+        <translation>Автоматически входить в сеансы после выхода</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="9"/>
+        <source>Name of session file for autologin session (if empty try last logged in)</source>
+        <translation>Название файла сеанса для автоматического входа</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="12"/>
+        <source>Username for autologin session</source>
+        <translation>Имя пользователя для автоматического входа</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="15"/>
+        <source>General</source>
+        <translation>Общие</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="18"/>
+        <source>Enable Qt&apos;s automatic high-DPI scaling</source>
+        <translation>Автомасштабирование для экранов с высоким разрешением (HiDPI)</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="21"/>
+        <source>Halt command</source>
+        <translation>Команда выключения</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="24"/>
+        <source>Input method module</source>
+        <translation>Модуль метода ввода</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="27"/>
+        <source>Initial NumLock state. Can be on, off or none.</source>
+        <translation>Начальное состояние NumLock. Может быть включен, выключен или ничего.</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="30"/>
+        <source>If property is set to none, numlock won&apos;t be changed
+NOTE: Currently ignored if autologin is enabled.</source>
+        <translation>Если параметр имеет значение ничего, numlock не будет изменен
+Примечание: В настоящее время игнорируется, если включен автовход.</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="34"/>
+        <source>Reboot command</source>
+        <translation>Команда перезагрузки</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="37"/>
+        <source>Theme</source>
+        <translation>Тема</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="40"/>
+        <source>Current theme name</source>
+        <translation>Название текущей темы</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="43"/>
+        <source>Cursor theme used in the greeter</source>
+        <translation>Тема курсора, используемая в приветствии</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="46"/>
+        <source>Number of users to use as threshold</source>
+        <translation>Пороговое значение количества пользователей</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="49"/>
+        <source>above which avatars are disabled
+unless explicitly enabled with EnableAvatars</source>
+        <translation>выше которого аватары отключены,
+если явно не разрешено с Включить аватары</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="53"/>
+        <source>Enable display of custom user avatars</source>
+        <translation>Включить отображение аватаров пользователей</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="56"/>
+        <source>Global directory for user avatars</source>
+        <translation>Глобальный каталог пользовательских аватаров</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="59"/>
+        <source>The files should be named &lt;username&gt;.face.icon</source>
+        <translation>Файлы должны называться &lt;имя_пользователя&gt;.face.icon</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="62"/>
+        <source>Theme directory path</source>
+        <translation>Путь к каталогу с темами</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="65"/>
+        <source>Users</source>
+        <translation>Пользователи</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="68"/>
+        <source>Default $PATH for logged in users</source>
+        <translation>$PATH по умолчанию для зарегистрированных пользователей</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="71"/>
+        <source>Comma-separated list of shells.</source>
+        <translation>Список командных оболочек через запятую.</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="74"/>
+        <source>Users with these shells as their default won&apos;t be listed</source>
+        <translation>Пользователи с этими командными оболочками по умолчанию не будут отображаться</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="77"/>
+        <source>Comma-separated list of users that should not be listed</source>
+        <translation>Список пользователей через запятые, которые не будут отображаться</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="80"/>
+        <source>Maximum user id for displayed users</source>
+        <translation>Максимальный идентификатор пользователя для отображаемых пользователей</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="83"/>
+        <source>Minimum user id for displayed users</source>
+        <translation>Минимальный идентификатор пользователя для отображаемых пользователей</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="86"/>
+        <source>Remember the session of the last successfully logged in user</source>
+        <translation>Запоминать сеанс последнего успешно зарегистрированного пользователя</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="89"/>
+        <source>Remember the last successfully logged in user</source>
+        <translation>Запоминать последнего успешно зарегистрированного пользователя</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="92"/>
+        <source>Wayland</source>
+        <translation>Wayland</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="95"/>
+        <location filename="src/cpp/config-strings.qml" line="122"/>
+        <source>Path to a script to execute when starting the desktop session</source>
+        <translation>Путь к сценарию, выполняемому при запуске сеанса рабочего стола</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="98"/>
+        <source>Directory containing available Wayland sessions</source>
+        <translation>Каталог, содержащий доступные сеансы Wayland</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="101"/>
+        <location filename="src/cpp/config-strings.qml" line="128"/>
+        <source>Path to the user session log file</source>
+        <translation>Путь к файлу журнала сеанса пользователя</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="104"/>
+        <source>X11</source>
+        <translation>X11</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="107"/>
+        <source>Path to a script to execute when starting the display server</source>
+        <translation>Путь к сценарию, выполняемому при запуске сервера дисплея</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="110"/>
+        <source>Path to a script to execute when stopping the display server</source>
+        <translation>Путь к сценарию, выполняемому при остановке сервера дисплея</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="113"/>
+        <source>The lowest virtual terminal number that will be used.</source>
+        <translation>Наименьший номер виртуального терминала, который будет использоваться.</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="116"/>
+        <source>Arguments passed to the X server invocation</source>
+        <translation>Аргументы, переданные вызову X-сервера</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="119"/>
+        <source>Path to X server binary</source>
+        <translation>Путь до двоичного файла X server</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="125"/>
+        <source>Directory containing available X sessions</source>
+        <translation>Каталог, содержащий доступные сеансы X</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="131"/>
+        <source>Path to the Xauthority file</source>
+        <translation>Путь к файлу Xauthority</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="134"/>
+        <source>Path to xauth binary</source>
+        <translation>Путь до двоичного файла xauth</translation>
+    </message>
+    <message>
+        <location filename="src/cpp/config-strings.qml" line="137"/>
+        <source>Path to Xephyr binary</source>
+        <translation>Путь до двоичного файла Xephyr</translation>
+    </message>
+</context>
+<context>
+    <name>file-editor</name>
+    <message>
+        <location filename="src/ui/file-editor.qml" line="27"/>
+        <location filename="src/cpp/ui/file-editor.qml" line="27"/>
+        <source>Please choose a %1</source>
+        <translation>Выберите %1</translation>
+    </message>
+</context>
+<context>
+    <name>main-window</name>
+    <message>
+        <location filename="src/ui/main-window.qml" line="8"/>
+        <location filename="src/cpp/ui/main-window.qml" line="8"/>
+        <source>SDDM Configuration Editor</source>
+        <translation>Редактор конфигурации SDDM</translation>
+    </message>
+    <message>
+        <location filename="src/ui/main-window.qml" line="30"/>
+        <location filename="src/cpp/ui/main-window.qml" line="30"/>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="src/ui/main-window.qml" line="45"/>
+        <location filename="src/cpp/ui/main-window.qml" line="45"/>
+        <source>Reset</source>
+        <translation>Сброс</translation>
+    </message>
+    <message>
+        <location filename="src/ui/main-window.qml" line="47"/>
+        <location filename="src/cpp/ui/main-window.qml" line="47"/>
+        <source>Load settings from the system configuation file</source>
+        <translation>Загрузить настройки из системного файла конфигурации</translation>
+    </message>
+    <message>
+        <location filename="src/ui/main-window.qml" line="51"/>
+        <location filename="src/cpp/ui/main-window.qml" line="51"/>
+        <source>Save...</source>
+        <translation>Сохранить...</translation>
+    </message>
+    <message>
+        <location filename="src/ui/main-window.qml" line="53"/>
+        <location filename="src/cpp/ui/main-window.qml" line="53"/>
+        <source>Save the settings to the system configuration file</source>
+        <translation>Сохранить настройки в системный файл конфигурации</translation>
+    </message>
+</context>
+<context>
+    <name>theme-editor</name>
+    <message>
+        <location filename="src/ui/theme-editor.qml" line="18"/>
+        <location filename="src/cpp/ui/theme-editor.qml" line="18"/>
+        <source>Preview</source>
+        <translation>Предпросмотр</translation>
+    </message>
+    <message>
+        <location filename="src/ui/theme-editor.qml" line="28"/>
+        <location filename="src/cpp/ui/theme-editor.qml" line="28"/>
+        <source>Theme Preview</source>
+        <translation>Предпросмотр оформления</translation>
+    </message>
+    <message>
+        <location filename="src/ui/theme-editor.qml" line="47"/>
+        <location filename="src/cpp/ui/theme-editor.qml" line="47"/>
+        <source>Displaying preview for SDDM theme &lt;b&gt;%1&lt;/b&gt;...</source>
+        <translation>Отображение предварительного просмотра для SDDM темы &lt;b&gt;%1&lt;/b&gt;...</translation>
+    </message>
+    <message>
+        <location filename="src/ui/theme-editor.qml" line="52"/>
+        <location filename="src/cpp/ui/theme-editor.qml" line="52"/>
+        <source>Close Preview</source>
+        <translation>Закрыть предпросмотр</translation>
+    </message>
+</context>
+</TS>
