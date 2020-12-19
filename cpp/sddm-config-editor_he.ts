490c739e73d1851b3abdeb4aa7f7f454c01a4ea4
Yaron Shahrabani <sh.yaron@gmail.com>
2020-12-19 13:06:28 +0100

    Weblate commits
    Co-authored-by: Yaron Shahrabani <sh.yaron@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/sddm-config-editor/he/
    Translation: LXQt Configuration/sddm-config-editor

--- cpp/sddm-config-editor_he.ts
+++ cpp/sddm-config-editor_he.ts
@@ -16,12 +16,12 @@
     <message>
         <location filename="config-strings.qml" line="9"/>
         <source>Name of session file for autologin session (if empty try last logged in)</source>
-        <translation type="unfinished"></translation>
+        <translation>שם קובץ ההפעלה לכניסה אוטומטית להפעלה (אם ריק לנסות את הכניסה האחרונה)</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="12"/>
         <source>Username for autologin session</source>
-        <translation type="unfinished"></translation>
+        <translation>שם משתמש לכניסה אוטומטית להפעלה</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="15"/>
@@ -31,12 +31,12 @@
     <message>
         <location filename="config-strings.qml" line="18"/>
         <source>Enable Qt&apos;s automatic high-DPI scaling</source>
-        <translation type="unfinished"></translation>
+        <translation>להפעיל את ההתאמה האוטומטית של Qt לרזולוציה גבוהה (Hi-DPI)</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="21"/>
         <source>Halt command</source>
-        <translation type="unfinished"></translation>
+        <translation>פקודת השבתה</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="24"/>
@@ -46,18 +46,19 @@
     <message>
         <location filename="config-strings.qml" line="27"/>
         <source>Initial NumLock state. Can be on, off or none.</source>
-        <translation type="unfinished"></translation>
+        <translation>מצב ה־NumLock ההתחלתי. יכול להיות on,‏ off או none (פעיל, כבוי או נטול החלטה).</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="30"/>
         <source>If property is set to none, numlock won&apos;t be changed
 NOTE: Currently ignored if autologin is enabled.</source>
-        <translation type="unfinished"></translation>
+        <translation>אם המאפיין הוגדר ל־none (נטול החלטה), מצב ה־numlock לא ישתנה
+לתשומת לבך: לא תקף אם מופעלת כניסה אוטומטית.</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="34"/>
         <source>Reboot command</source>
-        <translation type="unfinished"></translation>
+        <translation>פקודת הפעלה מחדש</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="37"/>
@@ -72,38 +73,39 @@ NOTE: Currently ignored if autologin is enabled.</source>
     <message>
         <location filename="config-strings.qml" line="43"/>
         <source>Cursor theme used in the greeter</source>
-        <translation type="unfinished"></translation>
+        <translation>ערכת המצביעים בה יעשה שימוש במקבל הפנים</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="46"/>
         <source>Number of users to use as threshold</source>
-        <translation type="unfinished"></translation>
+        <translation>מספר המשתמשים לשימוש בתור סף</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="49"/>
         <source>above which avatars are disabled
 unless explicitly enabled with EnableAvatars</source>
-        <translation type="unfinished"></translation>
+        <translation>מעבר לכך התמונות הייצוגיות יושבתו
+אלא אם כן הופעלו מפורשות עם EnableAvatars (הפעלת תמונות ייצוגיות)</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="53"/>
         <source>Enable display of custom user avatars</source>
-        <translation type="unfinished"></translation>
+        <translation>הפעלת הצגת תמונות ייצוגיות אישיות</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="56"/>
         <source>Global directory for user avatars</source>
-        <translation type="unfinished"></translation>
+        <translation>תיקייה גלובלית של תמונות משתמש ייצוגיות</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="59"/>
         <source>The files should be named &lt;username&gt;.face.icon</source>
-        <translation type="unfinished"></translation>
+        <translation>יש לקרוא לקבצים ‎ &lt;username&gt;.face.icon(להחליף בשם המשתמש)</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="62"/>
         <source>Theme directory path</source>
-        <translation type="unfinished"></translation>
+        <translation>נתיב תיקיית ערכות הנושא</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="65"/>
@@ -113,37 +115,37 @@ unless explicitly enabled with EnableAvatars</source>
     <message>
         <location filename="config-strings.qml" line="68"/>
         <source>Default $PATH for logged in users</source>
-        <translation type="unfinished"></translation>
+        <translation>‎$PATH בררת מחדל למשתמשים שנכנסו למערכת</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="71"/>
         <source>Comma-separated list of shells.</source>
-        <translation type="unfinished"></translation>
+        <translation>רשימת מערכות מעטפת מופרדת בפסיקים.</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="74"/>
         <source>Users with these shells as their default won&apos;t be listed</source>
-        <translation type="unfinished"></translation>
+        <translation>משתמשים עם מערכות מעטפת אלו כבררת מחדל לא יופיעו</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="77"/>
         <source>Comma-separated list of users that should not be listed</source>
-        <translation type="unfinished"></translation>
+        <translation>רשימה מופרדת בפסיקים של משתמשים שלא אמורים להופיע</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="80"/>
         <source>Maximum user id for displayed users</source>
-        <translation type="unfinished"></translation>
+        <translation>מזהה המשתמש המרבי למשתמשים המוצגים</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="83"/>
         <source>Minimum user id for displayed users</source>
-        <translation type="unfinished"></translation>
+        <translation>מזהה המשתמש המזערי למשתמשים המוצגים</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="86"/>
         <source>Remember the session of the last successfully logged in user</source>
-        <translation type="unfinished"></translation>
+        <translation>לזכור את ההפעלה של המשתמש האחרון שהצליח להיכנס</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="89"/>
@@ -159,18 +161,18 @@ unless explicitly enabled with EnableAvatars</source>
         <location filename="config-strings.qml" line="95"/>
         <location filename="config-strings.qml" line="122"/>
         <source>Path to a script to execute when starting the desktop session</source>
-        <translation type="unfinished"></translation>
+        <translation>נתיב לסקריפט שיופעל עם התחלת פעילות שולחן העבודה</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="98"/>
         <source>Directory containing available Wayland sessions</source>
-        <translation type="unfinished"></translation>
+        <translation>תיקייה שמכילה הפעלות Wayland זמינות</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="101"/>
         <location filename="config-strings.qml" line="128"/>
         <source>Path to the user session log file</source>
-        <translation type="unfinished"></translation>
+        <translation>נתיב לקובץ יומן הפעלת המשתמש</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="104"/>
@@ -180,47 +182,47 @@ unless explicitly enabled with EnableAvatars</source>
     <message>
         <location filename="config-strings.qml" line="107"/>
         <source>Path to a script to execute when starting the display server</source>
-        <translation type="unfinished"></translation>
+        <translation>נתיב לסקריפט שירוץ עם הפעלת שרת התצוגה</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="110"/>
         <source>Path to a script to execute when stopping the display server</source>
-        <translation type="unfinished"></translation>
+        <translation>נתיב לסקריפט שירוץ עם עצירת שרת התצוגה</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="113"/>
         <source>The lowest virtual terminal number that will be used.</source>
-        <translation type="unfinished"></translation>
+        <translation>מספר המסוף הווירטואלי הנמוך ביותר בו ייעשה שימוש.</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="116"/>
         <source>Arguments passed to the X server invocation</source>
-        <translation type="unfinished"></translation>
+        <translation>ארגומנטים שהועברו להפעלת שרת ה־X</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="119"/>
         <source>Path to X server binary</source>
-        <translation type="unfinished"></translation>
+        <translation>נתיב לבינרי של שרת ה־X</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="125"/>
         <source>Directory containing available X sessions</source>
-        <translation type="unfinished"></translation>
+        <translation>תיקייה שמכילה הפעלות X זמינות</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="131"/>
         <source>Path to the Xauthority file</source>
-        <translation type="unfinished"></translation>
+        <translation>נתיב לקובץ ה־Xauthority</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="134"/>
         <source>Path to xauth binary</source>
-        <translation type="unfinished"></translation>
+        <translation>נתיב לבינרי xauth</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="137"/>
         <source>Path to Xephyr binary</source>
-        <translation type="unfinished"></translation>
+        <translation>נתיב לבינרי של Xephyr</translation>
     </message>
 </context>
 <context>
@@ -243,7 +245,7 @@ unless explicitly enabled with EnableAvatars</source>
     <message>
         <location filename="main-window.qml" line="53"/>
         <source>Save...</source>
-        <translation>שמירה...</translation>
+        <translation>שמירה…</translation>
     </message>
 </context>
 <context>
