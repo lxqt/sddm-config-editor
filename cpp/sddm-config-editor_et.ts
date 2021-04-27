b8da77b4aa9f71728f806aa0c9be4cca17e8822f
Priit Jõerüüt <lxqtwlate@joeruut.com>
2021-04-28 00:07:15 +0200

    Weblate commits
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/sddm-config-editor/et/
    Translation: LXQt Configuration/sddm-config-editor

---
+++ cpp/sddm-config-editor_et.ts
@@ -0,0 +1,267 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>config-strings</name>
+    <message>
+        <location filename="config-strings.qml" line="3"/>
+        <source>Autologin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="6"/>
+        <source>Whether sddm should automatically log back into sessions when they exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="9"/>
+        <source>Name of session file for autologin session (if empty try last logged in)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="12"/>
+        <source>Username for autologin session</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="15"/>
+        <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="18"/>
+        <source>Enable Qt&apos;s automatic high-DPI scaling</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="21"/>
+        <source>Halt command</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="24"/>
+        <source>Input method module</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="27"/>
+        <source>Initial NumLock state. Can be on, off or none.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="30"/>
+        <source>If property is set to none, numlock won&apos;t be changed
+NOTE: Currently ignored if autologin is enabled.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="34"/>
+        <source>Reboot command</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="37"/>
+        <source>Theme</source>
+        <translation>Teema</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="40"/>
+        <source>Current theme name</source>
+        <translation>Praeguse teema nimi</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="43"/>
+        <source>Cursor theme used in the greeter</source>
+        <translation>Kursoriteema sisselogimisvaates</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="46"/>
+        <source>Number of users to use as threshold</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="49"/>
+        <source>above which avatars are disabled
+unless explicitly enabled with EnableAvatars</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="53"/>
+        <source>Enable display of custom user avatars</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="56"/>
+        <source>Global directory for user avatars</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="59"/>
+        <source>The files should be named &lt;username&gt;.face.icon</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="62"/>
+        <source>Theme directory path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="65"/>
+        <source>Users</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="68"/>
+        <source>Default $PATH for logged in users</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="71"/>
+        <source>Comma-separated list of shells.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="74"/>
+        <source>Users with these shells as their default won&apos;t be listed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="77"/>
+        <source>Comma-separated list of users that should not be listed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="80"/>
+        <source>Maximum user id for displayed users</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="83"/>
+        <source>Minimum user id for displayed users</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="86"/>
+        <source>Remember the session of the last successfully logged in user</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="89"/>
+        <source>Remember the last successfully logged in user</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="92"/>
+        <source>Wayland</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="95"/>
+        <location filename="config-strings.qml" line="122"/>
+        <source>Path to a script to execute when starting the desktop session</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="98"/>
+        <source>Directory containing available Wayland sessions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="101"/>
+        <location filename="config-strings.qml" line="128"/>
+        <source>Path to the user session log file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="104"/>
+        <source>X11</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="107"/>
+        <source>Path to a script to execute when starting the display server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="110"/>
+        <source>Path to a script to execute when stopping the display server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="113"/>
+        <source>The lowest virtual terminal number that will be used.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="116"/>
+        <source>Arguments passed to the X server invocation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="119"/>
+        <source>Path to X server binary</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="125"/>
+        <source>Directory containing available X sessions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="131"/>
+        <source>Path to the Xauthority file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="134"/>
+        <source>Path to xauth binary</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="137"/>
+        <source>Path to Xephyr binary</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main-window</name>
+    <message>
+        <location filename="main-window.qml" line="9"/>
+        <source>SDDM Configuration Editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>File</source>
+        <location filename="main-window.qml" line="32"/>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main-window.qml" line="47"/>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main-window.qml" line="53"/>
+        <source>Save...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>theme-editor</name>
+    <message>
+        <location filename="theme-editor.qml" line="18"/>
+        <source>Preview</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="theme-editor.qml" line="47"/>
+        <source>Displaying preview for SDDM theme &lt;b&gt;%1&lt;/b&gt;...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="theme-editor.qml" line="52"/>
+        <source>Close Preview</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
