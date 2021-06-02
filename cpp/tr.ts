8561e02195cce67a5d2296b741c11297889a354a
Aylinux <cihanalk@gmail.com>
2021-06-03 00:17:52 +0300

    Create tr.ts

---
+++ cpp/tr.ts
@@ -0,0 +1,269 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>config-strings</name>
+    <message>
+        <location filename="config-strings.qml" line="3"/>
+        <source>Autologin</source>
+        <translation>Otomatik giriş</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="6"/>
+        <source>Whether sddm should automatically log back into sessions when they exit</source>
+        <translation>Sddm'den, çıkıldığında otomatik olarak yeniden giriş yapıp yapmayacağı</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="9"/>
+        <source>Name of session file for autologin session (if empty try last logged in)</source>
+        <translation>Otomatik oturum açmak için oturum dosyasının adı (boşsa son oturum açılacak)</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="12"/>
+        <source>Username for autologin session</source>
+        <translation>Otomatik oturum açmak için kullanıcı adı</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="15"/>
+        <source>General</source>
+        <translation>Genel</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="18"/>
+        <source>Enable Qt&apos;s automatic high-DPI scaling</source>
+        <translation>Qt'nin otomatik yüksek DPI ölçeklendirmesini etkinleştir</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="21"/>
+        <source>Halt command</source>
+        <translation>Kapat komutu</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="24"/>
+        <source>Input method module</source>
+        <translation>Giriş yöntemi modülü</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="27"/>
+        <source>Initial NumLock state. Can be on, off or none.</source>
+        <translation>İlk NumLock durumu. Açık, kapalı veya hiçbiri olabilir. </translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="30"/>
+        <source>If property is set to none, numlock won&apos;t be changed
+NOTE: Currently ignored if autologin is enabled.</source>
+        <translation>Özellik yok olarak ayarlanırsa sayı kilidi değiştirilmez
+NOT: Otomatik oturum açma etkinse şu anda yok sayılır. </translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="34"/>
+        <source>Reboot command</source>
+        <translation>Yeniden başlat komutu</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="37"/>
+        <source>Theme</source>
+        <translation>Tema</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="40"/>
+        <source>Current theme name</source>
+        <translation>Aktif tema adı</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="43"/>
+        <source>Cursor theme used in the greeter</source>
+        <translation>Karşılayıcıda kullanılan imleç teması </translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="46"/>
+        <source>Number of users to use as threshold</source>
+        <translation>Eşik olarak kullanılacak kullanıcı sayısı </translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="49"/>
+        <source>above which avatars are disabled
+unless explicitly enabled with EnableAvatars</source>
+        <translation>EnableAvatars ile açıkça etkinleştirilmediği sürece 
+hangi avatarların devre dışı bırakıldığı </translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="53"/>
+        <source>Enable display of custom user avatars</source>
+        <translation>Özel kullanıcı avatarlarının görüntülenmesini etkinleştirin </translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="56"/>
+        <source>Global directory for user avatars</source>
+        <translation>Kullanıcı avatarları için global dizin </translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="59"/>
+        <source>The files should be named &lt;username&gt;.face.icon</source>
+        <translation>Dosyalar kullanıcı adı face.icon olarak adlandırılmalıdır </translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="62"/>
+        <source>Theme directory path</source>
+        <translation>Tema dizini yolu </translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="65"/>
+        <source>Users</source>
+        <translation>Kullanıcılar</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="68"/>
+        <source>Default $PATH for logged in users</source>
+        <translation>Giriş yapan kullanıcılar için varsayılan $PATH</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="71"/>
+        <source>Comma-separated list of shells.</source>
+        <translation>Kabukların virgülle ayrılmış listesi. </translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="74"/>
+        <source>Users with these shells as their default won&apos;t be listed</source>
+        <translation>Bu kabukları varsayılan olarak kullanan kullanıcılar listelenmeyecektir</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="77"/>
+        <source>Comma-separated list of users that should not be listed</source>
+        <translation>Listelenmemesi gereken kullanıcıların virgülle ayrılmış listesi</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="80"/>
+        <source>Maximum user id for displayed users</source>
+        <translation>Görüntülenen kullanıcılar için maksimum kullanıcı kimliği</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="83"/>
+        <source>Minimum user id for displayed users</source>
+        <translation>Görüntülenen kullanıcılar için minimum kullanıcı kimliği</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="86"/>
+        <source>Remember the session of the last successfully logged in user</source>
+        <translation>Başarıyla oturum açan son kullanıcının oturumunu hatırla</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="89"/>
+        <source>Remember the last successfully logged in user</source>
+        <translation>Başarıyla giriş yapan son kullanıcıyı hatırla</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="92"/>
+        <source>Wayland</source>
+        <translation>Wayland</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="95"/>
+        <location filename="config-strings.qml" line="122"/>
+        <source>Path to a script to execute when starting the desktop session</source>
+        <translation>Masaüstü oturumu başlatılırken yürütülecek komut dosyasının yolu</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="98"/>
+        <source>Directory containing available Wayland sessions</source>
+        <translation>Mevcut Wayland oturumlarını içeren dizin</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="101"/>
+        <location filename="config-strings.qml" line="128"/>
+        <source>Path to the user session log file</source>
+        <translation>Kullanıcı oturumu günlük dosyasının yolu</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="104"/>
+        <source>X11</source>
+        <translation>X11</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="107"/>
+        <source>Path to a script to execute when starting the display server</source>
+        <translation>Görüntü sunucusunu başlatırken yürütülecek komut dosyasının yolu</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="110"/>
+        <source>Path to a script to execute when stopping the display server</source>
+        <translation>Görüntü sunucusu durdurulduğunda yürütülecek komut dosyasının yolu</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="113"/>
+        <source>The lowest virtual terminal number that will be used.</source>
+        <translation>Kullanılacak en düşük sanal terminal numarası.</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="116"/>
+        <source>Arguments passed to the X server invocation</source>
+        <translation>X sunucusu çağrısına aktarılan bağımsız değişkenler</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="119"/>
+        <source>Path to X server binary</source>
+        <translation>X sunucusu ikili dosyasına giden yol</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="125"/>
+        <source>Directory containing available X sessions</source>
+        <translation>Mevcut X oturumlarını içeren dizin</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="131"/>
+        <source>Path to the Xauthority file</source>
+        <translation>Xauthority dosyasının yolu</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="134"/>
+        <source>Path to xauth binary</source>
+        <translation>Xauth ikili dosyasına giden yol</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="137"/>
+        <source>Path to Xephyr binary</source>
+        <translation>Xephyr ikili dosyasına giden yol</translation>
+    </message>
+</context>
+<context>
+    <name>main-window</name>
+    <message>
+        <location filename="main-window.qml" line="9"/>
+        <source>SDDM Configuration Editor</source>
+        <translation>SDDM Ayar Düzenleyicisi</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <location filename="main-window.qml" line="32"/>
+        <translation>Dosya</translation>
+    </message>
+    <message>
+        <location filename="main-window.qml" line="47"/>
+        <source>Reset</source>
+        <translation>Sıfırla</translation>
+    </message>
+    <message>
+        <location filename="main-window.qml" line="53"/>
+        <source>Save...</source>
+        <translation>Kaydet...</translation>
+    </message>
+</context>
+<context>
+    <name>theme-editor</name>
+    <message>
+        <location filename="theme-editor.qml" line="18"/>
+        <source>Preview</source>
+        <translation>Önizleme</translation>
+    </message>
+    <message>
+        <location filename="theme-editor.qml" line="47"/>
+        <source>Displaying preview for SDDM theme &lt;b&gt;%1&lt;/b&gt;...</source>
+        <translation>SDDM &lt;b&gt;%1&lt;/b&gt; teması için önizleme görüntüleniyor...</translation>
+    </message>
+    <message>
+        <location filename="theme-editor.qml" line="52"/>
+        <source>Close Preview</source>
+        <translation>Önizlemeyi Kapat</translation>
+    </message>
+</context>
+</TS>
