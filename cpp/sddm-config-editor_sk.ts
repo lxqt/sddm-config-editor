be31dcdf2c3997999cccded626a9357d4ddc2a94
pavol polonec <pavolpolonec@gmail.com>
2021-06-16 13:07:33 +0200

    Weblate commits
    Co-authored-by: pavol polonec <pavolpolonec@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/sddm-config-editor/sk/
    Translation: LXQt Configuration/sddm-config-editor

---
+++ cpp/sddm-config-editor_sk.ts
@@ -0,0 +1,269 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sk">
+<context>
+    <name>config-strings</name>
+    <message>
+        <location filename="config-strings.qml" line="3"/>
+        <source>Autologin</source>
+        <translation>Automatické prihlásenie</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="6"/>
+        <source>Whether sddm should automatically log back into sessions when they exit</source>
+        <translation>Nastaviť automatické prihlásenie do relácií po ich skončení</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="9"/>
+        <source>Name of session file for autologin session (if empty try last logged in)</source>
+        <translation>Názov súboru relácie pre automatické prihlásenie relácie (ak je pole prázdne, program skúsi posledné prihlásenie)</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="12"/>
+        <source>Username for autologin session</source>
+        <translation>Používateľské meno pre automatické prihlásenie</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="15"/>
+        <source>General</source>
+        <translation>Všeobecné</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="18"/>
+        <source>Enable Qt&apos;s automatic high-DPI scaling</source>
+        <translation>Povoliť automatickú úpravu DPI</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="21"/>
+        <source>Halt command</source>
+        <translation>Príkaz pre zastavenie</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="24"/>
+        <source>Input method module</source>
+        <translation>Modul vstupnej metódy</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="27"/>
+        <source>Initial NumLock state. Can be on, off or none.</source>
+        <translation>Počiatočný stav NumLock. Zapnúť, vypnúť alebo bez zmeny.</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="30"/>
+        <source>If property is set to none, numlock won&apos;t be changed
+NOTE: Currently ignored if autologin is enabled.</source>
+        <translation>Ak hodnotu nastavíte na &apos;bez zmeny&apos;, stav numerickej klávesnice sa nezmení
+POZN.: Toto nastavenie je momentálne ignorované pri zapnutí automatického prihlásenia.</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="34"/>
+        <source>Reboot command</source>
+        <translation>Príkaz na reštart</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="37"/>
+        <source>Theme</source>
+        <translation>Téma</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="40"/>
+        <source>Current theme name</source>
+        <translation>Súčasná téma</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="43"/>
+        <source>Cursor theme used in the greeter</source>
+        <translation>Vzhľad kurzora na uvítacej obrazovke</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="46"/>
+        <source>Number of users to use as threshold</source>
+        <translation>Limit používateľov</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="49"/>
+        <source>above which avatars are disabled
+unless explicitly enabled with EnableAvatars</source>
+        <translation>po jeho prekročení sa vypne funkcia avatarov
+ak nie je výslovne povolená pomocou EnabeAvatars</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="53"/>
+        <source>Enable display of custom user avatars</source>
+        <translation>Povoliť zobrazenie vlastných avatarov</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="56"/>
+        <source>Global directory for user avatars</source>
+        <translation>Globálny priečinok pre avatary používateľov</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="59"/>
+        <source>The files should be named &lt;username&gt;.face.icon</source>
+        <translation>Tieto súbory by ste mali nazvať &lt;meno_používateľa&gt;.face.icon</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="62"/>
+        <source>Theme directory path</source>
+        <translation>Cesta k priečinku s témami vzhľadu</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="65"/>
+        <source>Users</source>
+        <translation>Používatelia</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="68"/>
+        <source>Default $PATH for logged in users</source>
+        <translation>Predvolená premenná $PATH pre prihlásených používateľov</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="71"/>
+        <source>Comma-separated list of shells.</source>
+        <translation>Čiarkou oddelený zoznam shellov.</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="74"/>
+        <source>Users with these shells as their default won&apos;t be listed</source>
+        <translation>Užívatelia, ktorí majú tieto shelly predvolené nebudú vypísaní</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="77"/>
+        <source>Comma-separated list of users that should not be listed</source>
+        <translation>Čiarkou oddelený zoznam používateľov, ktorí nemajú byť vypísaní</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="80"/>
+        <source>Maximum user id for displayed users</source>
+        <translation>Maximálne ID používateľa zobrazených používateľov</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="83"/>
+        <source>Minimum user id for displayed users</source>
+        <translation>Minimálne ID používateľa zobrazených používateľov</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="86"/>
+        <source>Remember the session of the last successfully logged in user</source>
+        <translation>Pamätať si reláciu naposledy prihláseného používateľa</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="89"/>
+        <source>Remember the last successfully logged in user</source>
+        <translation>Pamätať si naposledy prihláseného používateľa</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="92"/>
+        <source>Wayland</source>
+        <translation>Wayland</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="95"/>
+        <location filename="config-strings.qml" line="122"/>
+        <source>Path to a script to execute when starting the desktop session</source>
+        <translation>Cesta k skriptu, ktorý sa vykoná pri spustení relácie</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="98"/>
+        <source>Directory containing available Wayland sessions</source>
+        <translation>Priečinok s dostupnými Wayland reláciami</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="101"/>
+        <location filename="config-strings.qml" line="128"/>
+        <source>Path to the user session log file</source>
+        <translation>Cesta k používateľskému logu</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="104"/>
+        <source>X11</source>
+        <translation>X11</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="107"/>
+        <source>Path to a script to execute when starting the display server</source>
+        <translation>Cesta ku skriptu, ktorý sa vykoná pri spustení zobrazovacieho servera</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="110"/>
+        <source>Path to a script to execute when stopping the display server</source>
+        <translation>Cesta ku skriptu, ktorý sa vykoná pri zastavení zobrazovacieho servera</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="113"/>
+        <source>The lowest virtual terminal number that will be used.</source>
+        <translation>Najnižšie možné číslo virtuálneho terminálu.</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="116"/>
+        <source>Arguments passed to the X server invocation</source>
+        <translation>Argumenty odovzdané pri volaní X serveru</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="119"/>
+        <source>Path to X server binary</source>
+        <translation>Cesta k spustiteľnému súboru x servera</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="125"/>
+        <source>Directory containing available X sessions</source>
+        <translation>Priečinok s dostupnými X reláciami</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="131"/>
+        <source>Path to the Xauthority file</source>
+        <translation>Cesta k súboru Xauthority</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="134"/>
+        <source>Path to xauth binary</source>
+        <translation>Cesta k spúšťateľnému súboru xauth</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="137"/>
+        <source>Path to Xephyr binary</source>
+        <translation>Cesta k spúšťateľnému súboru Xephyr</translation>
+    </message>
+</context>
+<context>
+    <name>main-window</name>
+    <message>
+        <location filename="main-window.qml" line="9"/>
+        <source>SDDM Configuration Editor</source>
+        <translation>Nastavenia SDDM</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <location filename="main-window.qml" line="32"/>
+        <translation>Súbor</translation>
+    </message>
+    <message>
+        <location filename="main-window.qml" line="47"/>
+        <source>Reset</source>
+        <translation>Vynulovať</translation>
+    </message>
+    <message>
+        <location filename="main-window.qml" line="53"/>
+        <source>Save...</source>
+        <translation>Uložiť...</translation>
+    </message>
+</context>
+<context>
+    <name>theme-editor</name>
+    <message>
+        <location filename="theme-editor.qml" line="18"/>
+        <source>Preview</source>
+        <translation>Náhľad</translation>
+    </message>
+    <message>
+        <location filename="theme-editor.qml" line="47"/>
+        <source>Displaying preview for SDDM theme &lt;b&gt;%1&lt;/b&gt;...</source>
+        <translation>Zobrazujem náhľad SDDM témy &lt;b&gt;%1&lt;/b&gt;...</translation>
+    </message>
+    <message>
+        <location filename="theme-editor.qml" line="52"/>
+        <source>Close Preview</source>
+        <translation>Zavrieť náhľad</translation>
+    </message>
+</context>
+</TS>
