a1c7815ccc5695659fb8afc0af6440f2511ac524
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-09-14 03:40:24 +0800

    Weblate commits (#57)
    Co-authored-by: Taras Panchenko <top39@ukr.net>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/sddm-config-editor/uk/
    Translation: LXQt Configuration/sddm-config-editor
    
    Co-authored-by: Taras Panchenko <top39@ukr.net>

---
+++ cpp/sddm-config-editor_uk.ts
@@ -0,0 +1,269 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk">
+<context>
+    <name>config-strings</name>
+    <message>
+        <location filename="config-strings.qml" line="3"/>
+        <source>Autologin</source>
+        <translation>Автоматичний вхід</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="6"/>
+        <source>Whether sddm should automatically log back into sessions when they exit</source>
+        <translation>Чи повинен sddm автоматично входити в сеанси після виходу з них</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="9"/>
+        <source>Name of session file for autologin session (if empty try last logged in)</source>
+        <translation>Назва файлу сеансу для автоматичного входу (якщо порожній, спробуйте останній вхід)</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="12"/>
+        <source>Username for autologin session</source>
+        <translation>Ім&apos;я користувача для автоматичного входу в сеанс</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="15"/>
+        <source>General</source>
+        <translation>Загальні</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="18"/>
+        <source>Enable Qt&apos;s automatic high-DPI scaling</source>
+        <translation>Увімкнути автоматичне масштабування Qt з високою роздільною здатністю</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="21"/>
+        <source>Halt command</source>
+        <translation>Припинити команду</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="24"/>
+        <source>Input method module</source>
+        <translation>Модуль способу введення</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="27"/>
+        <source>Initial NumLock state. Can be on, off or none.</source>
+        <translation>Початковий стан NumLock. Може бути on, off або none.</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="30"/>
+        <source>If property is set to none, numlock won&apos;t be changed
+NOTE: Currently ignored if autologin is enabled.</source>
+        <translation>Якщо встановлено значення none, NumLock не змінюється
+ПРИМІТКА: ігнорується, якщо ввімкнено автоматичний вхід.</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="34"/>
+        <source>Reboot command</source>
+        <translation>Команда для перевантаження</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="37"/>
+        <source>Theme</source>
+        <translation>Тема</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="40"/>
+        <source>Current theme name</source>
+        <translation>Назва поточної теми</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="43"/>
+        <source>Cursor theme used in the greeter</source>
+        <translation>Тема курсору для використання у greeter</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="46"/>
+        <source>Number of users to use as threshold</source>
+        <translation>Поріг кількості користувачів</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="49"/>
+        <source>above which avatars are disabled
+unless explicitly enabled with EnableAvatars</source>
+        <translation>які користувацькі зображення буде вимкнено,
+якщо явно не ввімкнено опцією EnableAvatars</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="53"/>
+        <source>Enable display of custom user avatars</source>
+        <translation>Увімкнути відображення користувацьких зображень</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="56"/>
+        <source>Global directory for user avatars</source>
+        <translation>Загальна тека для користувацьких зображень</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="59"/>
+        <source>The files should be named &lt;username&gt;.face.icon</source>
+        <translation>Файли повинні мати назву &lt;ім&apos;я_користувача&gt;.face.icon</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="62"/>
+        <source>Theme directory path</source>
+        <translation>Шлях до теки тем</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="65"/>
+        <source>Users</source>
+        <translation>Користувачі</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="68"/>
+        <source>Default $PATH for logged in users</source>
+        <translation>Типовий $PATH для користувачів, які увійшли</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="71"/>
+        <source>Comma-separated list of shells.</source>
+        <translation>Перелік оболонок, розділений комами.</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="74"/>
+        <source>Users with these shells as their default won&apos;t be listed</source>
+        <translation>Користувачі з цими оболонками, які є для них типовими, не будуть вказані</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="77"/>
+        <source>Comma-separated list of users that should not be listed</source>
+        <translation>Перелік користувачів, яких не має бути в переліку, розділений комами</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="80"/>
+        <source>Maximum user id for displayed users</source>
+        <translation>Максимально можливий UID користувача для відображених користувачів</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="83"/>
+        <source>Minimum user id for displayed users</source>
+        <translation>Мінімально можливий UID користувача для відображених користувачів</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="86"/>
+        <source>Remember the session of the last successfully logged in user</source>
+        <translation>Пам’ятати сеанс останнього користувача, який успішно зайшов</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="89"/>
+        <source>Remember the last successfully logged in user</source>
+        <translation>Пам’ятати останнього користувача, який успішно зайшов у сеанс</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="92"/>
+        <source>Wayland</source>
+        <translation>Wayland</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="95"/>
+        <location filename="config-strings.qml" line="122"/>
+        <source>Path to a script to execute when starting the desktop session</source>
+        <translation>Шлях до сценарію для виконання під час запуску стільниці</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="98"/>
+        <source>Directory containing available Wayland sessions</source>
+        <translation>Тека, що містить доступні сеанси Wayland</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="101"/>
+        <location filename="config-strings.qml" line="128"/>
+        <source>Path to the user session log file</source>
+        <translation>Шлях до файлу журналу сеансу користувача</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="104"/>
+        <source>X11</source>
+        <translation>X11</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="107"/>
+        <source>Path to a script to execute when starting the display server</source>
+        <translation>Шлях до сценарію для виконання під час запуску сервера відображення</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="110"/>
+        <source>Path to a script to execute when stopping the display server</source>
+        <translation>Шлях до сценарію для виконання під час зупинки сервера відображення</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="113"/>
+        <source>The lowest virtual terminal number that will be used.</source>
+        <translation>Найменший номер віртуального терміналу, який буде використовуватися.</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="116"/>
+        <source>Arguments passed to the X server invocation</source>
+        <translation>Аргументи, передані для виклику X сервера</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="119"/>
+        <source>Path to X server binary</source>
+        <translation>Шлях до виконуваного файлу X сервера</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="125"/>
+        <source>Directory containing available X sessions</source>
+        <translation>Тека з дійсними файлами X sessions</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="131"/>
+        <source>Path to the Xauthority file</source>
+        <translation>Шлях до файлу Xauthority</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="134"/>
+        <source>Path to xauth binary</source>
+        <translation>Шлях до виконуваного файлу xauth</translation>
+    </message>
+    <message>
+        <location filename="config-strings.qml" line="137"/>
+        <source>Path to Xephyr binary</source>
+        <translation>Шлях до виконуваного файлу Xephyr</translation>
+    </message>
+</context>
+<context>
+    <name>main-window</name>
+    <message>
+        <location filename="main-window.qml" line="9"/>
+        <source>SDDM Configuration Editor</source>
+        <translation>Редактор налаштувань SDDM</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <location filename="main-window.qml" line="32"/>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="main-window.qml" line="47"/>
+        <source>Reset</source>
+        <translation>Скинути</translation>
+    </message>
+    <message>
+        <location filename="main-window.qml" line="53"/>
+        <source>Save...</source>
+        <translation>Зберегти...</translation>
+    </message>
+</context>
+<context>
+    <name>theme-editor</name>
+    <message>
+        <location filename="theme-editor.qml" line="18"/>
+        <source>Preview</source>
+        <translation>Попередній перегляд</translation>
+    </message>
+    <message>
+        <location filename="theme-editor.qml" line="47"/>
+        <source>Displaying preview for SDDM theme &lt;b&gt;%1&lt;/b&gt;...</source>
+        <translation>Попередній перегляд теми SDDM&lt;b&gt;%1&lt;/b&gt;...</translation>
+    </message>
+    <message>
+        <location filename="theme-editor.qml" line="52"/>
+        <source>Close Preview</source>
+        <translation>Закрити попередній перегляд</translation>
+    </message>
+</context>
+</TS>
