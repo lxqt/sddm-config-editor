18cc391a8e4725f3b3837d0aceea47224cc3ea81
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-09-26 03:25:41 +0800

    Translations update from Weblate (#58)
    * Weblate commits
    
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/sddm-config-editor/uk/
    Translation: LXQt Configuration/sddm-config-editor
    
    * Weblate commits
    
    Co-authored-by: 이정희 <daemul72@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/sddm-config-editor/ko/
    Translation: LXQt Configuration/sddm-config-editor
    
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Co-authored-by: 이정희 <daemul72@gmail.com>

--- cpp/sddm-config-editor_ko.ts
+++ cpp/sddm-config-editor_ko.ts
@@ -1,156 +1,156 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="uk">
+<TS version="2.1" language="ko">
 <context>
     <name>config-strings</name>
     <message>
         <location filename="config-strings.qml" line="3"/>
         <source>Autologin</source>
-        <translation>Автоматичний вхід</translation>
+        <translation>자동로그인</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="6"/>
         <source>Whether sddm should automatically log back into sessions when they exit</source>
-        <translation>Чи повинен sddm автоматично входити в сеанси після виходу з них</translation>
+        <translation>세션이 종료될 때 sddm이 자동으로 세션에 다시 로그인해야 하는지 여부</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="9"/>
         <source>Name of session file for autologin session (if empty try last logged in)</source>
-        <translation>Назва файлу сеансу для автоматичного входу (якщо не вказано, спробувати останній вхід)</translation>
+        <translation>자동 로그인 세션의 세션 파일 이름(비어 있는 경우 마지막 로그인 시도)</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="12"/>
         <source>Username for autologin session</source>
-        <translation>Ім&apos;я користувача для автоматичного входу в сеанс</translation>
+        <translation>자동 로그인 세션의 사용자 이름</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="15"/>
         <source>General</source>
-        <translation>Загальні</translation>
+        <translation>일반</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="18"/>
         <source>Enable Qt&apos;s automatic high-DPI scaling</source>
-        <translation>Увімкнути автоматичне масштабування Qt з високою роздільною здатністю</translation>
+        <translation>Qt의 높은 DPI 자동 스케일링 활성화</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="21"/>
         <source>Halt command</source>
-        <translation>Припинити команду</translation>
+        <translation>중지 명령</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="24"/>
         <source>Input method module</source>
-        <translation>Модуль способу введення</translation>
+        <translation>입력기 모듈</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="27"/>
         <source>Initial NumLock state. Can be on, off or none.</source>
-        <translation>Початковий стан NumLock. Може бути on, off або none.</translation>
+        <translation>초기 NumLock 상태입니다. 켜짐, 꺼짐 또는 없음이 될 수 있습니다.</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="30"/>
         <source>If property is set to none, numlock won&apos;t be changed
 NOTE: Currently ignored if autologin is enabled.</source>
-        <translation>Якщо встановлено значення none, NumLock не змінюється
-ПРИМІТКА: ігнорується, якщо ввімкнено автоматичний вхід.</translation>
+        <translation>속성을 없음으로 설정하면 numlock이 변경되지 않습니다.
+참고: 자동 로그인이 활성화된 경우 현재 무시됩니다.</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="34"/>
         <source>Reboot command</source>
-        <translation>Команда для перезавантаження</translation>
+        <translation>재부팅 명령</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="37"/>
         <source>Theme</source>
-        <translation>Тема</translation>
+        <translation>테마</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="40"/>
         <source>Current theme name</source>
-        <translation>Назва поточної теми</translation>
+        <translation>현재 테마 이름</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="43"/>
         <source>Cursor theme used in the greeter</source>
-        <translation>Тема курсору для використання у greeter</translation>
+        <translation>인사말에 사용된 커서 테마</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="46"/>
         <source>Number of users to use as threshold</source>
-        <translation>Обмеження кількості користувачів</translation>
+        <translation>임계값으로 사용할 사용자 수</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="49"/>
         <source>above which avatars are disabled
 unless explicitly enabled with EnableAvatars</source>
-        <translation>які користувацькі зображення буде вимкнено,
-якщо явно не ввімкнено опцією EnableAvatars</translation>
+        <translation>아바타활성화를 사용하여 명시적으로 활성화되지
+않는 한 위의 아바타는 비활성화됩니다</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="53"/>
         <source>Enable display of custom user avatars</source>
-        <translation>Увімкнути показ користувацьких зображень</translation>
+        <translation>사용자 지정 사용자 아바타 화면표시 활성화</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="56"/>
         <source>Global directory for user avatars</source>
-        <translation>Загальна тека для користувацьких зображень</translation>
+        <translation>사용자 아바타의 전역 디렉터리</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="59"/>
         <source>The files should be named &lt;username&gt;.face.icon</source>
-        <translation>Файли повинні мати назву &lt;ім&apos;я_користувача&gt;.face.icon</translation>
+        <translation>파일 이름은 &lt;사용자이름&gt;.face.icon이어야 합니다</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="62"/>
         <source>Theme directory path</source>
-        <translation>Шлях до теки тем</translation>
+        <translation>테마 디렉터리 경로</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="65"/>
         <source>Users</source>
-        <translation>Користувачі</translation>
+        <translation>사용자</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="68"/>
         <source>Default $PATH for logged in users</source>
-        <translation>Типовий $PATH для користувачів, які увійшли</translation>
+        <translation>로그인한 사용자의 기본 $PATH</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="71"/>
         <source>Comma-separated list of shells.</source>
-        <translation>Перелік оболонок, розділений комами.</translation>
+        <translation>쉼표로 구분된 쉘 목록입니다.</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="74"/>
         <source>Users with these shells as their default won&apos;t be listed</source>
-        <translation>Не буде вказано користувачів, для яких ці оболонки типові</translation>
+        <translation>이러한 셸을 기본값으로 사용하는 사용자는 나열되지 않습니다</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="77"/>
         <source>Comma-separated list of users that should not be listed</source>
-        <translation>Перелік користувачів, яких не має бути в переліку, розділений комами</translation>
+        <translation>나열하면 안 되는 쉼표로 구분된 사용자 목록</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="80"/>
         <source>Maximum user id for displayed users</source>
-        <translation>Максимально можливий UID для показаних користувачів</translation>
+        <translation>화면표시된 사용자의 최대 사용자 ID</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="83"/>
         <source>Minimum user id for displayed users</source>
-        <translation>Мінімально можливий UID для показаних користувачів</translation>
+        <translation>화면표시된 사용자의 최소 사용자 ID</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="86"/>
         <source>Remember the session of the last successfully logged in user</source>
-        <translation>Пам’ятати сеанс останнього користувача, який успішно зайшов</translation>
+        <translation>마지막으로 성공적으로 로그인한 사용자의 세션 기억</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="89"/>
         <source>Remember the last successfully logged in user</source>
-        <translation>Пам’ятати останнього користувача, який успішно зайшов у сеанс</translation>
+        <translation>마지막으로 성공적으로 로그인한 사용자 기억</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="92"/>
@@ -161,18 +161,18 @@ unless explicitly enabled with EnableAvatars</source>
         <location filename="config-strings.qml" line="95"/>
         <location filename="config-strings.qml" line="122"/>
         <source>Path to a script to execute when starting the desktop session</source>
-        <translation>Шлях до сценарію для виконання під час запуску стільниці</translation>
+        <translation>바탕화면 세션을 시작할 때 실행할 스크립트 경로</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="98"/>
         <source>Directory containing available Wayland sessions</source>
-        <translation>Тека, що містить доступні сеанси Wayland</translation>
+        <translation>사용 가능한 Wayland 세션이 포함된 디렉터리</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="101"/>
         <location filename="config-strings.qml" line="128"/>
         <source>Path to the user session log file</source>
-        <translation>Шлях до файлу журналу сеансу користувача</translation>
+        <translation>사용자 세션 로그 파일의 경로</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="104"/>
@@ -182,47 +182,47 @@ unless explicitly enabled with EnableAvatars</source>
     <message>
         <location filename="config-strings.qml" line="107"/>
         <source>Path to a script to execute when starting the display server</source>
-        <translation>Шлях до сценарію для виконання під час запуску сервера відображення</translation>
+        <translation>디스플레이 서버를 시작할 때 실행할 스크립트 경로</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="110"/>
         <source>Path to a script to execute when stopping the display server</source>
-        <translation>Шлях до сценарію для виконання під час зупинки сервера відображення</translation>
+        <translation>디스플레이 서버를 중지할 때 실행할 스크립트 경로</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="113"/>
         <source>The lowest virtual terminal number that will be used.</source>
-        <translation>Найменший номер віртуального терміналу, який буде використовуватися.</translation>
+        <translation>사용할 가장 낮은 가상 터미널 번호입니다.</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="116"/>
         <source>Arguments passed to the X server invocation</source>
-        <translation>Аргументи, передані для виклику X сервера</translation>
+        <translation>X 서버 호출에 전달된 인수</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="119"/>
         <source>Path to X server binary</source>
-        <translation>Шлях до виконуваного файлу X сервера</translation>
+        <translation>X 서버 바이너리 경로</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="125"/>
         <source>Directory containing available X sessions</source>
-        <translation>Каталог з файлами доступних X-сеансів</translation>
+        <translation>사용 가능한 X 세션이 포함된 디렉터리</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="131"/>
         <source>Path to the Xauthority file</source>
-        <translation>Шлях до файлу Xauthority</translation>
+        <translation>Xauthority 파일의 경로</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="134"/>
         <source>Path to xauth binary</source>
-        <translation>Шлях до виконуваного файлу xauth</translation>
+        <translation>xauth 바이너리 경로</translation>
     </message>
     <message>
         <location filename="config-strings.qml" line="137"/>
         <source>Path to Xephyr binary</source>
-        <translation>Шлях до виконуваного файлу Xephyr</translation>
+        <translation>Xephyr 바이너리 경로</translation>
     </message>
 </context>
 <context>
@@ -230,22 +230,22 @@ unless explicitly enabled with EnableAvatars</source>
     <message>
         <location filename="main-window.qml" line="9"/>
         <source>SDDM Configuration Editor</source>
-        <translation>Редактор налаштувань SDDM</translation>
+        <translation>SDDM 구성 편집기</translation>
     </message>
     <message>
         <source>File</source>
         <location filename="main-window.qml" line="32"/>
-        <translation>Файл</translation>
+        <translation>파일</translation>
     </message>
     <message>
         <location filename="main-window.qml" line="47"/>
         <source>Reset</source>
-        <translation>Скинути</translation>
+        <translation>재설정</translation>
     </message>
     <message>
         <location filename="main-window.qml" line="53"/>
         <source>Save...</source>
-        <translation>Зберегти...</translation>
+        <translation>저장...</translation>
     </message>
 </context>
 <context>
@@ -253,17 +253,17 @@ unless explicitly enabled with EnableAvatars</source>
     <message>
         <location filename="theme-editor.qml" line="18"/>
         <source>Preview</source>
-        <translation>Попередній перегляд</translation>
+        <translation>미리보기</translation>
     </message>
     <message>
         <location filename="theme-editor.qml" line="47"/>
         <source>Displaying preview for SDDM theme &lt;b&gt;%1&lt;/b&gt;...</source>
-        <translation>Попередній перегляд теми SDDM&lt;b&gt;%1&lt;/b&gt;...</translation>
+        <translation>SDDM 테마 &lt;b&gt;%1&lt;/b&gt;의 미리보기를 화면표시하는 중...</translation>
     </message>
     <message>
         <location filename="theme-editor.qml" line="52"/>
         <source>Close Preview</source>
-        <translation>Закрити попередній перегляд</translation>
+        <translation>미리보기 닫기</translation>
     </message>
 </context>
 </TS>
